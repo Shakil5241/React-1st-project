6cfab9494ed950cbc115392fd859fa3a2dee2840
Shakil5241 <sk442664@gmail.com>
2022-03-11 12:40:49 +0600

    reactjs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './my components/Header';
+import { Todos } from './my components/Todos';
+import { Footer } from './my components/Footer';
+
+function App() {
+  return (
+   <>
+   <Header title="My Todos List"/>
+   <Todos/>
+   <Footer/>
+   </>
+  )};
+
+export default App;
+
+
